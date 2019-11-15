60a0756e8e91964604751b3d61311c0214b833fe
Darthsmith <piotrkowalik246@gmail.com>
2019-11-15 15:49:18 +0100

    ATM 0.1v (Deposit, check balance options)

--- src/sample/GUI.java
+++ src/sample/GUI.java
@@ -14,6 +14,7 @@ import static javafx.application.Platform.exit;
 class GUI {
 
     GUI(Operations operations) {
+        this.operations = operations;
         this.welcomeLabel();
         this.welcomeButton();
         this.mainATMLabel();
@@ -31,7 +32,6 @@ class GUI {
         this.depositAmountTextField();
         this.accountBalanceTextArea();
         this.addDepositButton();
-        this.operations = operations;
     }
 
     Button addDepositButton;
